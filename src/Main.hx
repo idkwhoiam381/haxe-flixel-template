6d15c5017b50e3160f9f1b9e4a20c5f9f551ee90
idkwhoiam381 <ismotass86@gmail.com>
2025-08-26 00:11:29 +0300

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -4,11 +4,11 @@ import flixel.FlxGame;
 
 import openfl.display.Sprite;
 
-import states.GameState;
+import states.MainMenuState;
 
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(1920, 1080, GameState, true));
+		addChild(new FlxGame(1920, 1080, MainMenuState, true));
 	}
 }
