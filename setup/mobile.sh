a0b58756505d2b01ea3065cc8e2c9ec354337ce6
idkwhoiam381 <ismotass86@gmail.com>
2025-08-26 10:11:30 +0300

    Update mobile.sh

--- setup/mobile.sh
+++ setup/mobile.sh
@@ -11,5 +11,5 @@ haxelib install flixel 4.10.0 --quiet
 haxelib install flixel-addons 3.1.1 --quiet
 haxelib install lime 8.0.2 --quiet
 haxelib install openfl 9.2.2 --quiet
-haxelib git hxcpp https://github.com/PsychExtendedThings/hxcpp --quiet
+haxelib install hxcpp 4.3.2 --quiet
 echo Finished!
